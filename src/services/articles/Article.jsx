462bb5b9b3959e1f159aa2ba966b2dda793d75a3
ABDELILAH <77416814+BrightProgrammer7@users.noreply.github.com>
2023-08-12 13:53:12 +0100

    Update Article.jsx

--- src/services/articles/Article.jsx
+++ src/services/articles/Article.jsx
@@ -8,7 +8,7 @@ function Article() {
   const getArticles = async () => {
     try {
       // const res = await axios.get('http://localhost:4000/')
-      const res = await axios.get('https://news-api-dxu3.onrender.com')
+      const res = await axios.get('https://netflix-news.onrender.com')
       setArticles(res.data)
     } catch (error) {
       console.log(error)
