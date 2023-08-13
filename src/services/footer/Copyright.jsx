c2490e4ef0005ff07a460689791fa5fcf805c633
ABDELILAH <77416814+BrightProgrammer7@users.noreply.github.com>
2023-08-13 21:56:26 +0100

    Update Copyright.jsx

--- src/services/footer/Copyright.jsx
+++ src/services/footer/Copyright.jsx
@@ -4,7 +4,7 @@ function Copyright() {
   return (
     <div className="py-5 text-secondText text-base font-normal flex-box">
       <CopyrightIcon />
-      2023 GPTechnews-All Right Reserved by Legend team
+      2023 GPTechNews-All Right Reserved by Legend Team
     </div>
   );
 }
