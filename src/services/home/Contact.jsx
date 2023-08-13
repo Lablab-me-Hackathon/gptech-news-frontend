e04284848de8c7df62046a857657175c5736dbf0
ABDELILAH <77416814+BrightProgrammer7@users.noreply.github.com>
2023-08-13 22:04:20 +0100

    Update Contact.jsx

--- src/services/home/Contact.jsx
+++ src/services/home/Contact.jsx
@@ -9,8 +9,8 @@ const Contact = () => {
       </div>
       <div className="flex flex-col text-secondText">
         <p className="font-semibold	">How can we help?</p>
-        <p className="m-1">+971 50 xxx xxxx</p>
-        <p>mohamad@gmail.com</p>
+        <p className="m-1">+971 50 000 0000</p>
+        <p>news.gptech@gmail.com</p>
       </div>
     </div>
   );
